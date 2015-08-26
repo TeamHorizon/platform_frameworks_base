54cd39e4db7e8d1f6b4b94ac60473ea690c03866
d34d <clark@cyngn.com>
2015-08-25 19:13:46 -0700

    Show icon of package associated with Toast
    For all those times you have some random app or background service
    that posts a Toast and you have no idea who's posting it.  This adds
    an icon badge to the top left corner of the Toast to show the app's
    icon the Toast belongs to.
    
    Change-Id: I82bf23664eea134f3b1f89ad5a99f6be73906ba8

--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -21,9 +21,11 @@ import android.annotation.StringRes;
 import android.app.INotificationManager;
 import android.app.ITransientNotification;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -402,17 +404,15 @@ public class Toast {
                 }
 
                 ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
-                if ((Settings.System.getInt(context.getContentResolver(), Settings.System.TOAST_ICON, 1) == 1)) {
-                    if (appIcon != null) {
-                        PackageManager pm = context.getPackageManager();
-                        Drawable icon = null;
-                        try {
-                            icon = pm.getApplicationIcon(packageName);
-                        } catch (PackageManager.NameNotFoundException e) {
-                            // nothing to do
-                        }
-                        appIcon.setImageDrawable(icon);
+                if (appIcon != null) {
+                    PackageManager pm = context.getPackageManager();
+                    Drawable icon = null;
+                    try {
+                        icon = pm.getApplicationIcon(packageName);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        // nothing to do
                     }
+                    appIcon.setImageDrawable(icon);
                 }
                 mWM = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
                 // We can resolve the Gravity here by using the Locale for getting
