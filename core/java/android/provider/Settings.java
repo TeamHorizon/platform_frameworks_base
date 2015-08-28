b1006468e75211c2a0ad3fd920113424ef2c550a
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    FWB: Toast icon switch (1/2)
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3730,12 +3730,11 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
-         * Volume rocker wake
          * @hide
          */
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
-		/**
+	/**
         * Navigation bar height when it is on protrait
         * @hide
         */
@@ -3813,6 +3812,12 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
+
+        /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
 
         /**
          * Switch for headset icon
