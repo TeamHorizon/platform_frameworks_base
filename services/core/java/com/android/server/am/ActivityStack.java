2374825200dd48c8baf4771ac9c801bb8594a6f8
tao.pei <tao.pei@ck-telecom.com>
2016-06-15 16:26:59 +0800

    am: Account for time changes when showing recent activities
    [Procedures]
    1. Go to Settings->Date&Time
       Set time backwards, eg. 2016.6.15 -> 2016.6.10
    
    2. Tap recent apps button to show recent activities.
       There will be no activities shown.
    
    Change-Id: Ifd860c0d08fac49f9d3c8eac590b6db9e6afb09d

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -4998,13 +4998,8 @@ final class ActivityStack {
             if (focusedStack && topTask) {
                 // Give the latest time to ensure foreground task can be sorted
                 // at the first, because lastActiveTime of creating task is 0.
-                long currentTime = System.currentTimeMillis();
-                // only update lastActiveTime if the currentTime is greater.
-                // The current time becomes lesser if the system date is changed to past.
-                // This FIXes the recents app not launching issue.
-                if(ci.lastActiveTime < currentTime) {
-                    ci.lastActiveTime = currentTime;
-                }
+                // Only do this if the clock didn't run backwards, though.
+                ci.lastActiveTime = Math.max(ci.lastActiveTime, System.currentTimeMillis());
                 topTask = false;
             }
 
